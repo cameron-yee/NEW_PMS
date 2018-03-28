71f158781a52c08f2361bf94bf970856ffe1315b
Carlos <carlos_garcia14@live.com>
2018-03-27 20:13:05 -0600

    modified model fields

--- pms/main/forms.py
+++ pms/main/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from . import models
 from .models import OrderDetail, Contract
+from django.utils.translation import gettext_lazy as _
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
@@ -14,23 +15,28 @@ class MyModelChoiceField(forms.ModelChoiceField): #gets the Contract name for th
         return obj.CName
 
 class PurchaseOrderForm(ModelForm):
-    CID = MyModelChoiceField(queryset=Contract.objects.all())
+    CID = MyModelChoiceField(queryset=Contract.objects.all()) #inserts the names of the contracts in the Contract choice fields
     class Meta: 
         model = models.OrderDetail
         fields = ['orderDate', 'CID', 'productName', 'productDescription', 'addressLine1', 'addressLine2', 'city', 'state', 'zipCode','quantity']
 
     def __init__(self, *args, **kwargs):
         super(PurchaseOrderForm, self).__init__(*args, **kwargs)
-        self.fields['CID'].label = 'Contract'
+        self.fields['CID'].label = 'Contract' #sets the label of the contracts as 'Contract"
+        self.fields['quantity'].widget.attrs['min'] = 1 #sets the minimum quanitity to 1
         instance = getattr(self, 'instance', None)
         if instance:
-            self.fields['orderDate'].widget.attrs['readonly'] = True
+            self.fields['orderDate'].widget.attrs['readonly'] = True #sets the orderDate field as readonly
 
     def __unicode__(self):
-        return self.fields['productName']
-            
+        return self.fields['productName'] #returns the product name as the object
             
 class QuoteForm(ModelForm):
     class Meta:
         model = models.Quote
         fields = ('Supplier', 'QPrice', 'QLink')
+
+    def __init__(self, *args, **kwargs): 
+        super(QuoteForm, self).__init__(*args, **kwargs)
+        self.fields['QPrice'].widget.attrs['min'] = 0.01 #sets the minimum allowable price to 0.01
+    
--- pms/main/models.py
+++ pms/main/models.py
@@ -1,49 +1,47 @@
-from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-
+from django.core.validators import MinValueValidator
 
 # Create your models here.
-
 class Quote(models.Model):
     QID = models.AutoField(primary_key=True, verbose_name= 'quote ID')
     OID = models.ForeignKey('OrderDetail', on_delete=models.CASCADE, related_name='+', verbose_name= 'order ID')
     QLink = models.CharField(max_length=1000, verbose_name= 'website Link')
-    QPrice = models.FloatField(verbose_name= 'item Price')
+    QPrice = models.DecimalField(max_digits=15, decimal_places=2, verbose_name= 'item Price')
     Supplier = models.CharField(max_length=30)
     def __str__(self):
-        return 'QID: {} {}'.format(self.OID, self.Supplier)
+        return 'QID: {} {}'.format(self.QID, self.Supplier)
 
 class Contract(models.Model):
     CID = models.AutoField(primary_key=True, verbose_name= 'contract ID')
     CName = models.CharField(max_length=30, verbose_name= 'contract Name')
-    CBudget = models.IntegerField(verbose_name= 'budget') 
+    CBudget = models.DecimalField(max_digits=20, decimal_places=2, verbose_name= 'budget') 
     CStart = models.DateField(verbose_name= 'contract Start Date')
     CEnd = models.DateField(null=True, verbose_name= 'contract End Date')
     def __str__(self):
         return '{}'.format(self.CName)
 
+
 class OrderDetail(models.Model):
-    OID = models.AutoField(primary_key=True, verbose_name = 'order ID')
+    OID = models.AutoField(primary_key=True, verbose_name = 'order #')
     EID = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, verbose_name = 'employee ID')
-    CID = models.ForeignKey(Contract, on_delete=models.CASCADE)
-    #null allows blank entry to be stored as null, blank allows the form to be saved without QID
-    QID = models.ForeignKey(Quote, on_delete=models.CASCADE, null=True, blank=True, verbose_name= 'quoteID')
-    productName = models.CharField(max_length=25, verbose_name= 'item Name')
+    CID = models.ForeignKey(Contract, on_delete=models.CASCADE, verbose_name='Contract') #null allows blank entry to be stored as null, blank allows the form to be saved without QID
+    QID = models.ForeignKey(Quote, on_delete=models.CASCADE, null=True, blank=True, verbose_name= 'quote')
+    productName = models.CharField(max_length=25, verbose_name= 'item')
     productDescription = models.CharField(max_length=200, verbose_name= 'description')
-    addressLine1 = models.CharField(max_length=40)
-    addressLine2 = models.CharField(max_length=40, null=True, blank=True)
+    addressLine1 = models.CharField(max_length=40, verbose_name='address Line 1')
+    addressLine2 = models.CharField(max_length=40, null=True, blank=True, verbose_name='address Line 2')
     city = models.CharField(max_length=50)
-    state = models.CharField(max_length=30)
-    zipCode = models.CharField(max_length=10, verbose_name= 'zip Code') #TODO: should be 5
-    quantity = models.IntegerField() #TODO: max quantity?
-    total = models.FloatField(verbose_name= 'order Total')
-    orderDate = models.DateField(default=timezone.now, verbose_name= 'date Requested')
+    state = models.CharField(max_length=25)
+    zipCode = models.CharField(max_length=5, verbose_name= 'zip Code')
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name= 'order Total')
+    orderDate = models.DateField(default=timezone.now, verbose_name= 'date of Request')
     dateApproved = models.DateField(null=True, blank=True, verbose_name= 'date Approved')
     dateReceived = models.DateField(null=True, blank=True, verbose_name= 'date Received')
     isPending = models.BooleanField(default=True, verbose_name= 'Pending')
     isDenied = models.BooleanField(default=False, verbose_name= 'Denied')
     isApproved = models.BooleanField(default=False, verbose_name= 'Approved')
     def __str__(self):
-        return '{}'.format(self.OID)
+        return '#{}'.format(self.OID)
