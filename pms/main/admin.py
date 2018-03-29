f248c818231e6d2964d2bfc16f9a279b5e8e6d51
Carlos <carlos_garcia14@live.com>
2018-03-28 21:22:58 -0600

    added functionality to remainingBudget on isDenied/isApproved

--- pms/main/admin.py
+++ pms/main/admin.py
@@ -1,5 +1,5 @@
 # from django.contrib import admin
-# from .models import OrderDetail, Contract, Quote
+# from .models import Order, Contract, Quote
 
 # class TaskAdmin(admin.ModelAdmin):
 #     list_display = ['CName', 'CBudget', 'CStart', 'CEnd']
@@ -12,22 +12,22 @@
 #     return field
 
 
-# admin.site.register(OrderDetail)
+# admin.site.register(Order)
 # admin.site.register(Contract, TaskAdmin)
 # admin.site.register(Quote)
 
 from django.contrib import admin
-from .models import OrderDetail, Contract, Quote
+from .models import Order, Contract, Quote
 
 class ContractAdmin(admin.ModelAdmin):
     list_display = ['CName', 'CBudget', 'CStart', 'CEnd']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['OID', 'EID', 'CID', 'productName', 'total', 'orderDate', 'QID']
+    list_display = ['OID', 'EID', 'CID', 'productName', 'total', 'orderDate', 'QID', 'isPending', 'isApproved', 'isDenied']
 
 class QuoteAdmin(admin.ModelAdmin):
     list_display = ['OID', 'QPrice', 'Supplier']
 
-admin.site.register(OrderDetail, OrderAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Contract, ContractAdmin)
 admin.site.register(Quote, QuoteAdmin)
--- pms/main/models.py
+++ pms/main/models.py
@@ -4,14 +4,15 @@ from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 # Create your models here.
 class Quote(models.Model):
     QID = models.AutoField(primary_key=True, verbose_name= 'quote ID')
-    OID = models.ForeignKey('OrderDetail', on_delete=models.CASCADE, related_name='+', verbose_name= 'order ID')
+    OID = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='+', verbose_name= 'order ID')
     QLink = models.CharField(max_length=1000, verbose_name= 'website Link')
-    QPrice = models.DecimalField(max_digits=15, decimal_places=2, verbose_name= 'item Price')
+    QPrice = models.DecimalField(max_digits=15, decimal_places=2, null=True, verbose_name= 'item Price')
     Supplier = models.CharField(max_length=30)
     def __str__(self):
         return 'QID: {} {}'.format(self.QID, self.Supplier)
@@ -19,14 +20,20 @@ class Quote(models.Model):
 class Contract(models.Model):
     CID = models.AutoField(primary_key=True, verbose_name= 'contract ID')
     CName = models.CharField(max_length=30, verbose_name= 'contract Name')
-    CBudget = models.DecimalField(max_digits=20, decimal_places=2, verbose_name= 'budget') 
+    CBudget = models.DecimalField(max_digits=10, decimal_places=2, verbose_name= 'budget')
+    remainingBudget = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='remaining Budget') 
     CStart = models.DateField(verbose_name= 'contract Start Date')
     CEnd = models.DateField(null=True, verbose_name= 'contract End Date')
+    def save(self, *args, **kwargs):
+        if self.remainingBudget is None:
+            self.remainingBudget = self.CBudget
+        super(Contract, self).save(*args, **kwargs)
+
     def __str__(self):
         return '{}'.format(self.CName)
 
 
-class OrderDetail(models.Model):
+class Order(models.Model):
     OID = models.AutoField(primary_key=True, verbose_name = 'order #')
     EID = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, verbose_name = 'employee ID')
     CID = models.ForeignKey(Contract, on_delete=models.CASCADE, verbose_name='Contract') #null allows blank entry to be stored as null, blank allows the form to be saved without QID
@@ -47,23 +54,29 @@ class OrderDetail(models.Model):
     isDenied = models.BooleanField(default=False, verbose_name= 'Denied')
     isApproved = models.BooleanField(default=False, verbose_name= 'Approved')
 
-    __original_denied_value = False #original link https://stackoverflow.com/questions/1355150/django-when-saving-how-can-you-check-if-a-field-has-changed
+    __original_denied_value = False
     __original_approved_value = False
     def __init__(self, *args, **kwargs):
-        super(OrderDetail, self).__init__(*args, **kwargs)
-        #self.__original_denied_value = self.isDenied
-       # self.__original_approved_value = self.isApproved
+        super(Order, self).__init__(*args, **kwargs)
     def save(self, force_insert=False, force_update=False, *args, **kwargs):
-        req_user = User.objects.get(username=self.EID)  
+        req_user = User.objects.get(username=self.EID) #gets the users information that requested the order 
+        contract_type = Contract.objects.get(CName=self.CID) #gets the contract that the order belongs to
         if self.isDenied != self.__original_denied_value:
+            if self.dateApproved != None: #checks if model has an approved date, if it does, then it adds total back to contract remainingBudget
+                contract_type.remainingBudget = contract_type.remainingBudget + self.total #readds the total that was previously taken away from contract remainingBudget
+                contract_type.save() #saves the contract model and readds the total the was previously taken away
+                self.dateApproved = None #resets the dateApproved field to empty
             send_mail(
                 'PURCHASE ORDER CONFIRMATION',
                 'Hi {},\n\nYou\'re recent purchase request for {} has been denied.\n\nPurchase Management System'.format(req_user.first_name, self.productName),
                 'yee.camero23@gmail.com', #Make info@system.com email
-                [req_user.email],
+                [req_user.email], #gets the email from the user that requested the item
                 fail_silently=False,
             )
         if self.isApproved != self.__original_approved_value:
+            contract_type.remainingBudget = contract_type.remainingBudget - self.total #subtracts the total from the remainingBudget
+            contract_type.save() #saves the contract model to show the new remainingBudget
+            self.dateApproved = datetime.now() #sets the approved date to now
             send_mail(
                 'PURCHASE ORDER CONFIRMATION',
                 'Hi {},\n\nYou\'re recent purchase request for {} has been Approved.\n\nPurchase Management System'.format(req_user.first_name, self.productName),
@@ -71,7 +84,7 @@ class OrderDetail(models.Model):
                 [req_user.email],
                 fail_silently=False,
             )
-        super(OrderDetail, self).save(force_insert, force_update, *args, **kwargs)
+        super(Order, self).save(force_insert, force_update, *args, **kwargs)
         self.__original_denied_value = self.isDenied
         self.__original_approved_value = self.isApproved
 
